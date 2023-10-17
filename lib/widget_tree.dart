0115cf195c8cac0875de3a8bbfe923543b252a97
nansah40 <nansah40@gmail.com>
2023-10-17 16:24:11 -0400

    added login screen

---
+++ lib/widget_tree.dart
@@ -0,0 +1,29 @@
+import 'package:dynamic_list/pages/home_page.dart';
+import 'package:dynamic_list/auth.dart';
+import 'package:dynamic_list/pages/login_register_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key ? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Homepage();
+        } else{
+          return const LoginPage();
+        }
+      }
+    );
+    
+  }
+}
